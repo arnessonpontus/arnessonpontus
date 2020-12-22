c56a0d8b187a375cc1c89ce466d2552757bafcb2
Pontus Arnesson <arnesson.pontus@gmail.com>
2020-12-22 13:49:45 +0100

    Small layout changes

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -33,16 +33,11 @@ const Layout: React.FC<Props> = ({ children }: Props) => {
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
+          paddingTop: 100,
         }}
       >
         <main>{children}</main>
-        <footer>
-          ©{new Date().getFullYear()}, Built with
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        <footer>©{new Date().getFullYear()} Pontus Arnesson</footer>
       </div>
     </>
   );
