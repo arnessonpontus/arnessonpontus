d704187886025c64689730f8d2245046f34a9339
Pontus Arnesson <arnesson.pontus@gmail.com>
2021-01-11 11:48:23 +0100

    Updates

---
+++ pages/page-2.js
@@ -0,0 +1,35 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = __importStar(require("react"));
+var gatsby_1 = require("gatsby");
+var layout_1 = __importDefault(require("../components/layout"));
+var seo_1 = __importDefault(require("../components/seo"));
+var SecondPage = function () { return (React.createElement(layout_1.default, null,
+    React.createElement(seo_1.default, { title: "Page two" }),
+    React.createElement("h1", null, "Hi from the second page"),
+    React.createElement("p", null, "Welcome to page 2"),
+    React.createElement(gatsby_1.Link, { to: "/" }, "Go back to the homepage"))); };
+exports.default = SecondPage;
+//# sourceMappingURL=page-2.js.map
