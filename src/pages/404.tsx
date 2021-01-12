732ec13d10e5b9cfed5682fe393d388ea43893df
Pontus Arnesson <arnesson.pontus@gmail.com>
2021-01-12 16:55:13 +0100

    Change simple 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,23 @@
 import * as React from 'react';
 
-import Layout from '../components/layout';
 import SEO from '../components/seo';
 
 const NotFoundPage: React.FC = () => (
-  <Layout>
+  <div
+    style={{
+      backgroundColor: '#0F2635',
+      height: '90vh',
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+  >
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <p>You just hit a route that doesn&#39;t exist...</p>
+  </div>
 );
 
 export default NotFoundPage;
